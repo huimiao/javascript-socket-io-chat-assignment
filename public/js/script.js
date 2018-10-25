87958654d316737923584f97de44767c2c908262
Saha <404110@cognizant.com>
2018-10-25 16:18:28 +0530

    Solution 1

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,115 @@
-function sendMessage() {
+function sendMessage(event, socket) {
+	event.preventDefault();
+
+	let channel = document.getElementById('channel').value;
+	let message = document.getElementById('message').value;
+	let username = document.getElementById('username').value;
+
+	let msg = `Me : ${message}`;
+	generateChatMsgTemplate(msg);
+	socket.emit('message', { username, channel, message });
+
 }
 
-function joinChannel() {
+function joinChannel(event, socket) {
+	event.preventDefault();
+	let channel = document.getElementById('newchannel').value;
+
+	//console.log('isWelcomeDisplayed' + isWelcomeDisplayed);
+	if (channel && isWelcomeDisplayed) {
+		socket.emit('joinChannel', { channel });
+	}
+
 }
 
-function leaveChannel() {
+function leaveChannel(event, socket) {
+	event.preventDefault();
+	let channel = document.getElementById('newchannel').value;
+
+	if (channel) {
+		socket.emit('leaveChannel', { channel });
+	}
 }
 
-function onWelcomeMessageReceived() {
+var isWelcomeDisplayed = false;
+function onWelcomeMessageReceived(welcomeMessage) {
+	let msg = `System : ${welcomeMessage}`;
+
+	isWelcomeDisplayed = true;
+	generateChatMsgTemplate(msg);
 }
 
-function onNewMessageReceived() {
+function onNewMessageReceived(newMessage) {
+	let msg = `${newMessage.username} : ${newMessage.message}`;
+	generateChatMsgTemplate(msg);
 }
 
-function onAddedToNewChannelReceived() {
+function onAddedToNewChannelReceived(data) {
+	let channel = data.channel;
+
+	let alertMessage = `You are added to <strong>${channel}</strong> successfully!`;
+	let content = generateAlertMsgTemplate(alertMessage);
+	document.getElementById('alertContainer').innerHTML += content;
+
+	let select = document.getElementById('channelsList');
+	let option = document.createElement("option")
+	option.text = `${channel}`;
+	select.appendChild(option);
 }
 
-function onRemovedFromChannelReceived() {
+function onRemovedFromChannelReceived(data) {
+
+	let channel = data.channel;
+
+	let alertMessage = `You are removed from <strong>${channel}</strong> successfully!`;
+	let content = generateAlertMsgTemplate(alertMessage);
+	document.getElementById('alertContainer').innerHTML += content;
+
+	let select = document.getElementById('channelsList');
+	let optionList = select.options;
+
+	let optionIndexToRemove;
+	for (let i = 0; i < optionList.length; i++) {
+		let option = optionList.options[i];
+
+		if (option.value === channel) {
+			optionIndexToRemove = i;
+		}
+	}
+
+	select.removeChild(optionIndexToRemove);
 }
+
+function generateChatMsgTemplate(msg) {
+	var content = `
+	<div class="col-12">
+		<div class="card received-message">
+			<div class="card-body">
+				<p class="card-text">${msg}</p>
+			</div>
+		</div>
+	</div>
+	`;
+
+	let initialcontent = document.getElementById('chatContainer').innerHTML;
+	let finalContent = content + initialcontent;
+	document.getElementById('chatContainer').innerHTML = finalContent;
+
+	return content;
+}
+
+function generateAlertMsgTemplate(msg) {
+	let content = `
+		<div class="alert alert-success alert-dismissible fade show" role="alert">
+			${msg}
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+	`;
+	return content;
+}
+
 
 module.exports = {
 	sendMessage,
--- server.js
+++ server.js
@@ -1,6 +1,55 @@
 function bootstrapSocketServer(io) {
-	io.on('connection', () => {
-	});
+	io.on('connection', (socket) => {
+		console.log('a user has connected');
+
+		socket.on('register', (message) => {
+			let username = message.username;
+			let channels = message.channels;
+
+			if (username && username !== 'Anonymous') {
+				socket.emit('welcomeMessage', `Welcome ${message.username} !!`);
+
+				channels.forEach(channel => {
+
+					if(channel) {
+						socket.join(channel);
+						socket.emit('addedToChannel', {channel}); ///?????
+					}
+					
+				});
+
+			} 
+		});
+
+		socket.on('message', (newMessage) => {
+			let message = newMessage.message;
+			let channel = newMessage.channel;
+			let username = newMessage.username;
+
+			if(message && channel) {
+				socket.broadcast.emit('newMessage', { username, channel, message });
+			}
+		});
+
+		socket.on('joinChannel', (newChannel) => {
+			let channel = newChannel.channel;
+			socket.join(channel);//socket join channel -- ??
+
+			if(channel) {
+				socket.emit('addedToChannel', {channel});
+			}
+		});
+
+		socket.on('leaveChannel', (newChannel) => {
+			let channel = newChannel.channel;
+			socket.leave(channel);//socket join channel -- ??
+
+			if(channel) {
+				socket.emit('removedFromChannel', {channel});
+			}
+		});
+
+	})
 }
 
 module.exports = bootstrapSocketServer;
